4d1588d016975b656c8db35061a2f4189b37d4d0
Mohit Jangra <mohitjangra12@gmail.com>
2023-09-17 08:16:37 +0530

    fixed .gitignore, was preventing configs from getting added

---
+++ after/plugin/indentblankline.lua
@@ -0,0 +1,16 @@
+-- configuration for whitespace characters [[ nextLine or enter, space, multispace ]]
+vim.opt.list = true
+vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "space: "
+vim.opt.listchars:append "lead:∙" -- lead space replacements
+vim.opt.listchars:append "trail: "
+vim.opt.listchars:append "extends:>"
+vim.opt.listchars:append "precedes:<"
+vim.opt.termguicolors = true
+
+require('indent_blankline').setup {
+    show_current_context = true,
+    show_current_context_start = true,
+    show_end_of_line = true,
+    space_char_blankline = " "
+}
