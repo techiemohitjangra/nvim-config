edfd9aec5c72ce99999b8d3379dec59a8b5c03dc
Mohit Jangra <mohitjangra12@gmail.com>
2023-11-25 03:08:05 +0530

    UPDATE: updated config for nvim-autopair to automatically add corresponding closing bracket or quote

--- lua/mohitjangra/packer.lua
+++ lua/mohitjangra/packer.lua
@@ -223,7 +223,16 @@ return require('packer').startup(function(use)
     -- use("jiangmiao/auto-pairs")
     use({
         "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
+        dependencies = { 'hrsh7th/nvim-cmp' },
+        config = function()
+            require("nvim-autopairs").setup {}
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on(
+                'confirm_done',
+                cmp_autopairs.on_confirm_done()
+            )
+        end
     })
 
 end)
