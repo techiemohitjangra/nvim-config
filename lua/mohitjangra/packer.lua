5fa0a51cd24612254a0d9c695134aa677195fd52
Mohit Jangra <mohitjangra12@gmail.com>
2023-12-31 15:57:39 +0530

    refreshed packer packages file packer.lua

--- lua/mohitjangra/packer.lua
+++ lua/mohitjangra/packer.lua
@@ -228,6 +228,7 @@ return require('packer').startup(function(use)
             require("nvim-autopairs").setup {}
             local cmp_autopairs = require('nvim-autopairs.completion.cmp')
             local cmp = require('cmp')
+
             cmp.event:on(
                 'confirm_done',
                 cmp_autopairs.on_confirm_done()
