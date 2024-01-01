fd82b72cae46098ec3122b2a083763722f245d6c
Mohit Jangra <mohitjangra12@gmail.com>
2024-01-01 18:22:21 +0530

    updated markdown keybinds to use code for non-ascii character instead of special characters as they were causing errors

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,23 +4,20 @@ function Action(macro)
     vim.fn.winrestview(old_pos)
 end
 
--- -- mark list item done
--- vim.keymap.set("n", "md", [[:lua action("^f[€ýalrx")<CR>]])
-vim.keymap.set("n", "md", [[:lua Action("^f[\128\253alrx")<CR>]])
+-- mark list item done
+vim.keymap.set("n", "md", ":lua Action('^f[\\128\\253alrx')<CR>")
 
 -- Mark list item not done (alternative representation)
--- vim.keymap.set("n", "mnd", [[:lua action("^f[€ýalr ")<CR>]])
-vim.keymap.set("n", "mnd", [[:lua Action("^f[\128\253alr ")<CR>]])
+vim.keymap.set("n", "mnd", ":lua Action('^f[\\128\\253alr ')<CR>")
 
 -- Mark list item done and move item to end of file
--- vim.keymap.set("n", "mD", [[:lua action("€ü0f[€ýalrx€ýaddGp")<CR>]])
-vim.keymap.set("n", "mD", [[:lua Action("\128\2520f[\128\253alrx\128\253addGp")<CR>]])
+vim.keymap.set("n", "mD", ":lua Action('^f[\\128\\253alrx\\128\\253addGp')<CR>")
 
 -- Make current line a list item
-vim.keymap.set("n", "mi", [[:lua Action("^i- [ ] \<Esc>")<CR>]])
+vim.keymap.set("n", "mi", ":lua Action('^i- [ ] \\27')<CR>")
 
 -- Add items in the next line
-vim.keymap.set("n", "ma", [[:lua Action("o- [ ] \<Esc>")<CR>]])
+vim.keymap.set("n", "ma", ":lua Action('o- [ ] \\27')<CR>")
 
 -- Add items in the previous line
-vim.keymap.set("n", "mA", [[:lua Action("O- [ ] \<Esc>")<CR>]])
+vim.keymap.set("n", "mA", ":lua Action('O- [ ] \\27')<CR>")
