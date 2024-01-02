236ff971bc48583da76f4939ab6024c3a81d4555
Mohit Jangra <mohitjangra12@gmail.com>
2024-01-02 20:15:57 +0530

    Update: quality of life update for adding new check mark item in a list in markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,6 +4,10 @@ function Action(macro)
     vim.fn.winrestview(old_pos)
 end
 
+function NewItem(macro)
+    vim.cmd(string.format(":normal! %s", macro))
+end
+
 -- mark list item done
 vim.keymap.set("n", "md", ":lua Action('^f[\\128\\253alrx')<CR>")
 
@@ -17,7 +21,7 @@ vim.keymap.set("n", "mD", ":lua Action('^f[\\128\\253alrx\\128\\253addGp')<CR>")
 vim.keymap.set("n", "mi", ":lua Action('^i- [ ] \\27')<CR>")
 
 -- Add items in the next line
-vim.keymap.set("n", "ma", ":lua Action('o- [ ] \\27')<CR>")
+vim.keymap.set("n", "ma", ":lua NewItem('o- [ ] \\27')<CR>")
 
 -- Add items in the previous line
-vim.keymap.set("n", "mA", ":lua Action('O- [ ] \\27')<CR>")
+vim.keymap.set("n", "mA", ":lua NewItem('O- [ ] \\27')<CR>")
