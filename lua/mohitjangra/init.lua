87481c37ccc482b6176d6d6993bf789f3ca91270
Mohit Jangra <mohitjangra12@gmail.com>
2024-01-13 09:45:40 +0530

    added diagnostic.lua to init.lua

--- lua/mohitjangra/init.lua
+++ lua/mohitjangra/init.lua
@@ -1,5 +1,6 @@
 require("mohitjangra.remaps")
 require("mohitjangra.set")
+require("mohitjangra.diagnostic")
 
 local MohitJangraGroup = vim.api.nvim_create_augroup('mohitjangra', {})
 local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
