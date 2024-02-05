14cfef549b72f0d3d69cc886a535ea7363c758da
Mohit Jangra <mohitjangra12@gmail.com>
2024-02-06 01:12:30 +0530

    Indent_Blankline: added listchars for tab and changed listchars for precedes and extends

--- lua/mohitjangra/plugins/indent_blankline.lua
+++ lua/mohitjangra/plugins/indent_blankline.lua
@@ -5,11 +5,26 @@ return {
         vim.opt.list = true
 
         -- vim.opt.listchars:append "eol:↴"
+
         vim.opt.listchars:append "space: "
+        -- vim.opt.listchars:append "space:␣"
+
+        -- vim.opt.listchars:append "tab:  "
+        -- vim.opt.listchars:append "tab:»■"
+        vim.opt.listchars:append "tab:▸\\"
+
+
         -- vim.opt.listchars:append "lead:∙" -- lead space replacements
+
         vim.opt.listchars:append "trail: "
-        vim.opt.listchars:append "extends:>"
-        vim.opt.listchars:append "precedes:<"
+        -- vim.opt.listchars:append "trail:·"
+
+        vim.opt.listchars:append "extends:→"
+        -- vim.opt.listchars:append "extends:»"
+
+        vim.opt.listchars:append "precedes:←"
+        -- vim.opt.listchars:append "precedes:«"
+
         vim.opt.termguicolors = true
 
         require('ibl').setup {
