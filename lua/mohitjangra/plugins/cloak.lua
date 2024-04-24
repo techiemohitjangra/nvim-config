d91738790d2afcafe2b28f008ddd27971822d9cb
Mohit Jangra <mohitjangra12@gmail.com>
2024-04-24 06:14:13 +0530

    CLOAK: enabled cloak

--- lua/mohitjangra/plugins/cloak.lua
+++ lua/mohitjangra/plugins/cloak.lua
@@ -1,5 +1,20 @@
 return {
     'laytan/cloak.nvim',
     config = function()
+        require("cloak").setup({
+            enable = true,
+            cloak_character = "*",
+            highlight_group = "Comment",
+            patterns = {
+                {
+                    -- set file to cloak to work with
+                    file_pattern = {
+                        ".env",
+                        ".dev.vars",
+                    },
+                    cloak_pattern = "=.+",
+                },
+            },
+        })
     end
 }
