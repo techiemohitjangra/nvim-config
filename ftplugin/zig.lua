0a1eaf94a773d1301c748134bad2fa013fa51263
Mohit Jangra <mohitjangra12@gmail.com>
2024-10-03 19:40:36 +0530

    updated zig build keybind to build and run, also added keybind to build and to run unit tests, also added keybind for release build

--- ftplugin/zig.lua
+++ ftplugin/zig.lua
@@ -1,2 +1,11 @@
--- add keybind to build Zig project
-vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":!zig build<CR>", { noremap = true, silent = true })
+-- add keybind to build and run Zig project
+vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":!zig build run<CR>",
+    { noremap = true, silent = true, desc = "Build and run zig project" })
+
+-- add keybind to build and test Zig project
+vim.api.nvim_buf_set_keymap(0, "n", "<F6>", ":!zig build test<CR>",
+    { noremap = true, silent = true, desc = "Build and run unit test" })
+
+-- add keybind to build and test Zig project
+vim.api.nvim_buf_set_keymap(0, "n", "<F9>", ":!zig build --release=",
+    { noremap = true, silent = true, desc = "Release Build zig project" })
