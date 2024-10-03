a8ae66afae8da792f8cf18ae9a081a88ca8a4862
Mohit Jangra <mohitjangra12@gmail.com>
2024-10-03 19:52:04 +0530

    updated build and run keybind to perform the command in a terminal on the right

--- ftplugin/zig.lua
+++ ftplugin/zig.lua
@@ -1,5 +1,5 @@
 -- add keybind to build and run Zig project
-vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":!zig build run<CR>",
+vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":rightbelow vsplit | term zig build run; exit<CR>i",
     { noremap = true, silent = true, desc = "Build and run zig project" })
 
 -- add keybind to build and test Zig project
