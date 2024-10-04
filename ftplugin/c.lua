f44e353b4465a154c6459e2ffb0fe3490edbaa6b
Mohit Jangra <mohitjangra12@gmail.com>
2024-10-04 16:28:59 +0530

    updated C specific keybind to compile the program in a separate terminal panal

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,8 +1,9 @@
 -- add \ at the end of line, use for macros in c and c++
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>cm", "A\\<ESC>j0", { noremap = true, silent = true })
 
--- add keybind to compile C/C++ single file
-vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":!gcc -Wall % -o %< && %<<CR>", { noremap = true, silent = true })
+-- add keybind to compile and execute C files
+vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":rightbelow vsplit | term gcc -Wall % -o %< && %<; exit<CR>i",
+    { noremap = true, silent = true })
 
 -- comment docs
 -- Example:
