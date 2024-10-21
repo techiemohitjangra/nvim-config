75d9ac96147ed15389d7e7cd441e7d602e147898
Mohit Jangra <mohitjangra12@gmail.com>
2024-10-21 13:58:17 +0530

    changed keymap for yank to the end of line to the system clipboard and added keymap to yank entier line to the system clipboard

--- lua/mohitjangra/remaps.lua
+++ lua/mohitjangra/remaps.lua
@@ -38,7 +38,8 @@ vim.keymap.set("n", "<leader>P", [["+P]])
 
 -- yank to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>yy", [["+yy]])
+vim.keymap.set("n", "<leader>Y", [["+y$]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
